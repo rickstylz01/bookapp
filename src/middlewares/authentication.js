60d82fc0670edc73559813f46b34914c8dd09a37
rickstylz01 <rickmaya@gmail.com>
2021-06-01 12:01:49 -0500

    remove extra next() method from authentication.js file

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,10 +1,24 @@
+const jwt = require('jsonwebtoken');
+const secret = "verySecureSecret";
+
 exports.authenticateUser = (req, res, next) => {
 	// check if there is an authorization token
 	if (!req.headers.authorization) {
 		return res.status(401).json({message: "authorization header required"});
 	}
-	next()
+	let splittedHeader = req.headers.authorization.split(' ');
+	if (splittedHeader[0] !== "Bearer") {
+		return res.status(401).json({message: "authorization format is Bearer <token>"})
+	}
+	let token = splittedHeader[1];
 	// decode token
-	// check if valid
-	// allow user to continue with request
+	jwt.verify(token, secret, (err, decodedToken) => {
+		if (err) return res.status(500).json({err});
+		// check if valid
+		if (!decodedToken) {
+			return res.status(401).json({message: "invalid authorization token. please login"})
+		}
+		// allow user to continue with request 
+		next();
+	})
 }
